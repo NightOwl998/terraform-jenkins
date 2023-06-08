91a370313ebcdacb07036fac2d1e1ec4e1ebb888
fadia <fadia@XLIM05040.sic.univ-poitiers.fr>
2023-06-08 17:57:11 +0200

    added agent1

--- instances.tf
+++ instances.tf
@@ -1,3 +1,10 @@
+data "aws_secretsmanager_secret" "db_psswd" {
+  name = "rds/password"
+}
+data "aws_secretsmanager_secret_version" "db_psswd" {
+  secret_id = data.aws_secretsmanager_secret.db_psswd.id
+
+}
 resource "aws_iam_role" "main" {
   name                = var.env_code
   managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"]
@@ -39,13 +46,15 @@ resource "aws_instance" "jenkins" {
     name = "jenkins"
   }
 }
-resource "aws_instance" "production" {
+resource "aws_instance" "agent1" {
   ami                         = "ami-0688ba7eeeeefe3cd" #"ami-06e85d4c3149db26a"
   instance_type               = "t2.micro"
   subnet_id                   = module.vpc.public_subnets[0]
   vpc_security_group_ids      = [aws_security_group.public.id]
   associate_public_ip_address = true
   iam_instance_profile        = aws_iam_instance_profile.my-inst-profile.name
+  user_data                   = base64encode(templatefile("user-data2.tpl", { password = jsondecode(data.aws_secretsmanager_secret_version.db_psswd.secret_string)["password"] }))
+
   tags = {
     name = "production"
   }
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,16 @@
-output "public_ip" {
+output "jenkins_cp_public_ip" {
   value = aws_instance.jenkins.public_ip
 }
-output "production_ip" {
-  value = aws_instance.production.public_ip
+output "jenkins_cp_private_ip" {
+  value = aws_instance.jenkins.private_ip
+}
+
+output "agent1_public_ip" {
+  value = aws_instance.agent1.public_ip
+}
+
+output "agent1_private_ip" {
+  value = aws_instance.agent1.private_ip
 }
 output "elb" {
   value = module.lb.elb_dns_name
